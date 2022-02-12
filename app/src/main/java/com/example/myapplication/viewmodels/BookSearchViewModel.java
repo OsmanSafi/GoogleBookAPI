d135b22e7773dc8f4c73a4ab1fd04c1a992130a0
OsmanSafi <osmansafi1027@gmail.com>
2022-02-11 19:55:54 -0800

    pushing homework 1

---
+++ app/src/main/java/com/example/myapplication/viewmodels/BookSearchViewModel.java
@@ -0,0 +1,36 @@
+package com.example.myapplication.viewmodels;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.myapplication.models.VolumesResponse;
+import com.example.myapplication.repository.BookRepository;
+
+//import io.github.cdimascio.dotenv.Dotenv; this did not work-___-
+
+public class BookSearchViewModel extends AndroidViewModel{
+    private BookRepository bookRepository;
+    private LiveData<VolumesResponse> volumesResponseLiveData;
+
+    public BookSearchViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public void init() {
+        bookRepository = new BookRepository();
+        volumesResponseLiveData = bookRepository.getVolumesResponseLiveData();
+    }
+
+    public void searchVolumes(String keyword, String author) {
+       // Dotenv dotenv = Dotenv.configure().directory("/assets").filename("env").load();
+        bookRepository.searchVolumes(keyword, author, "AIzaSyBD7QDht99PhXnl71z3g91bV2rgBClOjHs");
+    }
+
+    public LiveData<VolumesResponse> getVolumesResponseLiveData() {
+        return volumesResponseLiveData;
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication/views/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myapplication.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.example.myapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
